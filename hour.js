630f68a50d0ed2b2e9029b847890d70340b7d945
İpek Nur Günümdoğdu <90203389+ladyvahsi@users.noreply.github.com>
2022-09-13 15:56:49 +0300

    Add files via upload
    Html + Css+ Js ile oluşturulmuş basit renkli analog saat yapımı.

---
+++ hour.js
@@ -0,0 +1,22 @@
+'use strict'
+const sec =document.querySelector(".secs");
+const min =document.querySelector(".mins");
+const hours =document.querySelector(".hours");
+
+function tiktak () {
+	let second = new Date ().getSeconds();
+	let minute = new Date ().getMinutes();
+	let hour = new Date ().getHours();
+
+	sec.style.transform = `rotate(${180 + (second * 6)}deg)`
+	min.style.transform = `rotate(${180 + (minute * 6)}deg)`
+	hours.style.transform = `rotate(${180 + (hour * 30)}deg)`
+	
+
+	console.log({second,minute,hour})
+	
+}
+
+setInterval(tiktak, 1000); 
+
+
